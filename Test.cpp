44c15a4a071d77f7359a2357b54c007156cbb450
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-06-18 22:07:19 -0400

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello Docker container!" << std::endl;
+  //std::cout << "Hello Docker container!" << std::endl;
+  printf("Hello World")
   return 0;
 }
